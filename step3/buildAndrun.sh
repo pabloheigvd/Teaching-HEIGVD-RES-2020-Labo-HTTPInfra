e66fe77da4343e8d6ec55533f0288f2d214c06f4
Sachouille <sacha.perdrizat@gmail.com>
2020-05-29 15:00:38 +0200

    add dynamic configuration and correct script

--- step3/buildAndrun.sh
+++ step3/buildAndrun.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 imageName='res/apache-reverse-proxy'
+
 containerName='res-rp'
+staticContainerName='res-static'
+dynamicContainerName='res-dyn'
 
 docker build  -t $imageName .
 
-if [ -n $(docker ps -aq --filter "name=${containerName}") ]
+if [ -n "$(docker ps -aq --filter "name=${containerName}")" ]
 then
   echo "suppression du container existant"
   docker rm $(docker stop $(docker ps -aq --filter "name=${containerName}"))
 fi
 
-docker run -d -p 8080:80 --name $containerName $imageName
+ip_Static=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $staticContainerName)
+ip_Dynamic=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $dynamicContainerName)
+
+docker run -e HTTP_STATIC=${ip_Static}:80 -e HTTP_DYNAMIQUE=${ip_Dynamic}:3000 -p 8080:80 --name $containerName $imageName
